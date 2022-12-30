62fa1c3efe87a54544c966db0bbc5eb658e70764
AviGautam1 <avifetian@gmail.com>
2022-12-29 22:13:19 -0800

    Write a Python program to check the nth-1 string is a proper substring of nth string in a given list of strings.

--- Program-5.py
+++ Program-5.py
@@ -6,6 +6,11 @@ is a proper substring of nth string in a given list of strings.
 str = input("Enter A String : ")
 sub_str = input("Enter A Substring : ")
 
+'''
+The find() method finds the first occurrence of the specified value. 
+The find() method returns -1 if the value is not found.
+'''
+
 if str.find(sub_str) == -1:
     print("Not Found !")
 
